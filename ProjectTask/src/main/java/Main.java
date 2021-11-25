1d2e3c55d34132e66cee9ec5c3c5492066635470
Danila <55926566+danilazamotov@users.noreply.github.com>
2021-11-25 23:47:11 +0300

    Add files via upload

---
+++ ProjectTask/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aboutstudent", "root","3lou_777");
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * from asd");
+
+            while (resultSet.next()) {
+
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
